e249562627b2ea2a1b730f0dd1132b5fd912d0e5
robocodebr <robocodebr@gmail.com>
2020-11-01 19:11:16 +0300

    lesson 8

---
+++ heh/scripts/scripts.js
@@ -0,0 +1,24 @@
+console.log("hello");
+
+let i = 0;
+
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+while(i<=20){
+    console.log(i);
+    i++;
+}
+
+let d = 20;
+
+while(d>0){
+    console.log(d);
+    d = d - 1;
+}
+
+for(let t = 0; t<10; t++){
+    console.log(t);
+}
